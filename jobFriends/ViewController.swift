471d29d4bf05d8a6ab0ad5725c2d0d4aca1be2b8
Boris Khomut <boris.khomut@gmail.com>
2015-03-30 17:49:09 -0400

    Initial Commit

---
+++ jobFriends/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jobFriends
+//
+//  Created by Boris Khomut on 3/30/15.
+//  Copyright (c) 2015 Boris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
