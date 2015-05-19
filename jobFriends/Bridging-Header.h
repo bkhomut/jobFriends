349077bf8daf31cb62bcd95ab2de95fc471c2cae
Boris Khomut <boris.khomut@gmail.com>
2015-05-19 14:08:29 -0400

    it's been a while

---
+++ jobFriends/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  jobFriends
+//
+//  Created by Boris Khomut on 4/3/15.
+//  Copyright (c) 2015 Boris. All rights reserved.
+//
+
+#ifndef jobFriends_Bridging_Header_h
+#define jobFriends_Bridging_Header_h
+
+
+#endif
---
+++ jobFriends/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  jobFriends
+//
+//  Created by Boris Khomut on 4/22/15.
+//  Copyright (c) 2015 Boris. All rights reserved.
+//
+
+import Foundation
